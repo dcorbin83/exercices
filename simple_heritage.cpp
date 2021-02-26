d66f5464621a2afcdb3093ba6fce63ab87c56678
dcorbin83 <67876528+dcorbin83@users.noreply.github.com>
2021-02-26 18:58:53 +0100

    Update simple_heritage.cpp

--- simple_heritage.cpp
+++ simple_heritage.cpp
@@ -8,35 +8,41 @@ class A
     
     virtual ~A(){}
     
-    virtual void foo(){ std::cout << _a  << "\n";}
+    virtual void foo() const { std::cout << "A\n";}
     
     int getX() const { return _x;}  ;
 
      protected:
       int _x;  
-      char * _a = "A";
+  
 };
 
-class B: public A
+class B final : public A
 {
   public: 
-   B(int x) : A(x) {
-       _a = "B";}
+   B(int x) : A(x) {}
    B(int x, int y) : A(x) , _y(y) {}
+   
+   void foo() const override { std::cout << "B\n";}
 
-    
+  void static bar(){};
    protected:
       int _y; 
       
 };
 
+void f1( const A& a){
+   a.foo();
+}
 int main(void)
 {
    B *b = new B(5);
    //move contructor 
    A &a = *b;
    
+   b->foo(); // should display B
    a.foo(); // should display B
+   f1(a); // should display B
    delete b;
    
    return 0;
