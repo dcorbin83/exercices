7c5fd39f1a5ae435045e65eb7e1c76ff9dad4d41
dcorbin83 <67876528+dcorbin83@users.noreply.github.com>
2021-05-06 17:59:40 +0200

    Update matrice.cpp

--- matrice.cpp
+++ matrice.cpp
@@ -1,52 +1,45 @@
 #include <iostream>
+
 using namespace std;
 
+template < class T >
+  class Matrice {
 
-template <class T>
-struct Point {
+    public:
+      Matrice(int raw, int cols): _raw(raw), _cols(cols) {
 
-  T x;
-  T y;
-};
-template <class T>
-class Matrice {
-  
-  public:
-  Matrice(int raw , int cols) : _raw(raw) , _cols(cols) {
-    
-    Data = new int*[_raw];
-    for (int i = 0; i < _raw; ++i)
+        Data = new int * [_raw];
+        for (int i = 0; i < _raw; ++i)
           Data[i] = new int[_cols];
 
-  } 
-  
-  T getPoint(int x , int y){
-    return Data[x][y];  
-  }
-  void setPoint(int x , int y, T v ){
-     Data[x][y] = v;
-  }
- virtual  ~Matrice(){
-    
-    for (int i = 0; i < _raw; ++i){
-      delete [] Data[i];
+      }
+
+    T getValAt(int x, int y) {
+      return Data[x][y];
     }
-    delete [] Data;
-  }
- 
-  protected:
-  int _raw;
-  int _cols;
-  Point<T>  p;
-  int ** Data ;
-  private:
-  size_t size;
-};
+    void setValAt(int x, int y, T v) {
+      Data[x][y] = v;
+    }
+    virtual~Matrice() {
+
+      for (int i = 0; i < _raw; ++i) {
+        delete[] Data[i];
+      }
+      delete[] Data;
+    }
+
+    protected:
+      int _raw;
+    int _cols;
+    T ** Data;
+    private:
+      size_t size;
+  };
 
 int main() {
-	Matrice<int> m(5,5);
-  
-  m.setPoint(2,2,10);
- std::cout << m.getPoint(2,2);
-	return 0;
+  Matrice < int > m(5, 5);
+
+  m.setValAt(2, 2, 10);
+  std::cout << m.getValAt(2, 2);
+  return 0;
 }
