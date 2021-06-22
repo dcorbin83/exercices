3ef794dbc099812bc55879ea77558285674c0ce8
dcorbin83 <67876528+dcorbin83@users.noreply.github.com>
2021-06-22 11:09:03 +0200

    Update Complex.cpp

--- Complex.cpp
+++ Complex.cpp
@@ -5,14 +5,24 @@ using namespace std;
 template <class T>
 struct Z {
   Z(T im , T re) : _im(im) , _re(re){}
-  Z operator*(Z<T> z1){
+  virtual ~Z(){};
+  Z operator*(const Z<T>& z1){
   return Z( (z1._im * this->_im)*-1 ,  z1._re * this->_re  );
   }  
+  T getIm() const { return _im;}
+  T getRe() const { return _re;}
   
   void toPrint() const { std::cout << _im << "i ;" << _re << std::endl;  }
+private:
   T _im;
   T _re;
+  
 };
+
+template <class T>
+  std::ostream& operator<<(std::ostream& out, const Z<T>& z){
+   return out << z.getIm() << "i ;" << z.getRe() << std::endl;
+}
 
 int main() {
   
@@ -28,9 +38,9 @@ int main() {
   
   // override * 
   Z<int> zP =  vz.at(0) * vz.at(1);
-  zP.toPrint();
+  //zP.toPrint();
   
   //bonus ovveride << 
- 
+  std::cout << zP;
 	return 0;
 }
