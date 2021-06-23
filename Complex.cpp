235ac93af970a23c571a48f9dd40882ae5708c0e
dcorbin83 <67876528+dcorbin83@users.noreply.github.com>
2021-06-23 14:58:42 +0200

    fix multiplication

--- Complex.cpp
+++ Complex.cpp
@@ -6,13 +6,12 @@ template <class T>
 struct Z {
   Z(T im , T re) : _im(im) , _re(re){}
   virtual ~Z(){};
-  Z operator+(const Z<T>& z1){
-  return Z( (z1._im + this->_im) ,  z1._re + this->_re  );
+  Z operator*(const Z<T>& z1){
+  return Z( z1._im * this->_re + z1._re * this->_im ,  z1._re * this->_re - z1._im * this->_im );
   }  
   T getIm() const { return _im;}
   T getRe() const { return _re;}
   
-  void toPrint() const { std::cout << _im << "i ;" << _re << std::endl;  }
 private:
   T _im;
   T _re;
@@ -28,17 +27,17 @@ int main() {
   
   //using template
   std::vector<Z<int>> vz;
-  vz.push_back(Z(1,2));
-  vz.push_back(Z(2,3));
+  vz.push_back(Z(4,1));
+  vz.push_back(Z(1,5));
   
   // display vector
   for(const auto&  e : vz){
-    e.toPrint();
+    std::cout << e ;
   }
   
   // override * 
-  Z<int> zP =  vz.at(0) + vz.at(1);
-
-  std::cout << zP;
+  Z<int> zP =  vz.at(0) * vz.at(1);
+ std::cout << zP ;
+  
 	return 0;
 }
